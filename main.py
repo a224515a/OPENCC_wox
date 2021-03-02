b3d7b7426d7f1f907f10435940135bb75b255018
a224515a <a224515a@gmail.com>
2021-03-02 16:25:58 +0800

    style(typo): typo

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from opencc import OpenCC
 import clipboard
 from wox import Wox, WoxAPI
 
-class Main(Wox):
 
+class Main(Wox):
     def copy(self, text):
-	
+
         clipboard.copy(text)
-		
+
     def query(self, param):
-	
+
         results = []
-		
+
         if not param:
             results.append({
                 "Title": '簡繁轉換',
@@ -22,18 +22,20 @@ class Main(Wox):
 
         if len(param.split(':')) == 2:
             code = param.split(':')[1]
-            if code.endswith("s2t"):			
+            if code.endswith("s2t"):
                 code = "s2t"
-            elif code.endswith("s2tw"):			
+            elif code.endswith("s2tw"):
                 code = "s2tw"
             elif code.endswith("s2twp"):
                 code = "s2twp"
+            elif code.endswith("s2twpf"):
+                code = "s2twpf"				
             elif code.endswith("hk2s"):
                 code = "hk2s"
             elif code.endswith("s2hk"):
                 code = "s2hk"
             elif code.endswith("t2hk"):
-                code = "t2hk"			
+                code = "t2hk"
             elif code.endswith("t2s"):
                 code = "t2s"
             elif code.endswith("t2tw"):
@@ -46,22 +48,21 @@ class Main(Wox):
                 code = "s2twp"
         else:
             code = "s2twp"
-            
+
         cc = OpenCC(code)
         converted = cc.convert(param.split(':')[0])
-		
+
         results.append({
             "Title": converted,
             "SubTitle": code,
-            "IcoPath": "Images/"+code+".png",
+            "IcoPath": "Images/" + code + ".png",
             'JsonRPCAction': {
                 'method': 'copy',
                 'parameters': [converted]
             }
-            })
+        })
 
         return results
-
 
 
 if __name__ == "__main__":
