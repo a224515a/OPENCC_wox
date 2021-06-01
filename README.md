b26d9364212f94ffeb99851fef182769f408dd5c
a224515a <a224515a@gmail.com>
2021-06-01 22:10:28 +0800

    refactor!: 改採官方 python接口
    BREAKING CHANGE: 改採官方 python接口且將轉換格式逐一列出以供使用者選擇

--- README.md
+++ README.md
@@ -4,41 +4,40 @@
 ## Getting Started
 
   OpenCC (Open Chinese Convert, 開放中文轉換) 是相當知名的中文簡繁轉換開源項目。
-  [yichen0831](https://github.com/yichen0831/opencc-python) 將其改寫為純 Python程式碼，可相當輕鬆的整合到 Wox。
-
+  ~~[yichen0831](https://github.com/yichen0831/opencc-python) 將其改寫為純 Python程式碼~~透過官方提供的 [Python接口](https://pypi.org/project/OpenCC/)，可相當輕鬆的整合到 Wox。
 
 ### Installing
 
 將資料夾存放在 ..\AppData\Roaming\Wox\Plugins\ 下即可使用。
 
-(需先安裝 `opencc-python`)
-> `pip install opencc-python-reimplemented`
+(需先安裝 ~~`opencc-python`~~ `opencc`)
+> ~~`pip install opencc-python-reimplemented`~~
+> `pip install OpenCC`
 
-### Usage 
+### Usage
 
-- 輸入關鍵詞 (`opencc`) 及簡體中文，直接轉換為臺灣正體(符合台灣常用詞彙)。
-- 輸入關鍵詞 (`opencc`) 及目標文字，並在 `:` 後指定轉換格式。
+- 輸入關鍵詞 (`cc`) 及目標文字，直接轉換為臺灣正體 (符合台灣常用詞彙)。
+- 輸入關鍵詞 (`cc`) 及轉換格式後輸入目標文字。
+- 輸入關鍵詞 (`cc`) 選取轉換格式後輸入目標文字。
 
 #### Conversions
-  * `hk2s`: Traditional Chinese (Hong Kong standard) to Simplified Chinese
 
-  * `s2hk`: Simplified Chinese to Traditional Chinese (Hong Kong standard)
+  * `s2t`<img src="Images/s2t.png" style="zoom:25%;" />: Simplified Chinese to Traditional Chinese 簡體到繁體
+  * `t2s`<img src="Images/t2s.png" style="zoom:25%;" />: Traditional Chinese to Simplified Chinese 繁體到簡體
+  * `s2tw`<img src="Images/s2tw.png" style="zoom:25%;" />: Simplified Chinese to Traditional Chinese (Taiwan Standard) 簡體到臺灣正體
+  * `tw2s`<img src="Images/tw2s.png" style="zoom:25%;" />: Traditional Chinese (Taiwan Standard) to Simplified Chinese 臺灣正體到簡體
+  * `s2hk`<img src="Images/s2hk.png" style="zoom:25%;" />: Simplified Chinese to Traditional Chinese (Hong Kong variant) 簡體到香港繁體
+  * `hk2s`<img src="Images/hk2s.png" style="zoom:25%;" />: Traditional Chinese (Hong Kong variant) to Simplified Chinese 香港繁體到簡體
+  * `s2twp`<img src="Images/s2twp.png" style="zoom:25%;" />: Simplified Chinese to Traditional Chinese (Taiwan Standard) with Taiwanese idiom 簡體到繁體（臺灣正體標準）並轉換爲臺灣常用詞彙
+  * `tw2sp`<img src="Images/tw2sp.png" style="zoom:25%;" />: Traditional Chinese (Taiwan Standard) to Simplified Chinese with Mainland Chinese idiom 繁體（臺灣正體標準）到簡體並轉換爲中國大陸常用詞彙
+  * `t2tw`<img src="Images/t2tw.png" style="zoom:25%;" />: Traditional Chinese (OpenCC Standard) to Taiwan Standard 繁體（OpenCC 標準）到臺灣正體
+  * `hk2t`<img src="Images/hk2t.png" style="zoom:25%;" />: Traditional Chinese (Hong Kong variant) to Traditional Chinese 香港繁體到繁體（OpenCC 標準）
+  * `t2hk`<img src="Images/t2hk.png" style="zoom:25%;" />: Traditional Chinese (OpenCC Standard) to Hong Kong variant 繁體（OpenCC 標準）到香港繁體
+  * `t2jp`<img src="Images/t2jp.png" style="zoom:25%;" />: Traditional Chinese Characters (Kyūjitai) to New Japanese Kanji (Shinjitai) 繁體（OpenCC 標準，舊字體）到日文新字體
+  * `jp2t`<img src="Images/jp2t.png" style="zoom:25%;" />: New Japanese Kanji (Shinjitai) to Traditional Chinese Characters (Kyūjitai) 日文新字體到繁體（OpenCC 標準，舊字體）
+  * `tw2t`<img src="Images/tw2t.png" style="zoom:25%;" />: Traditional Chinese (Taiwan standard) to Traditional Chinese 臺灣正體到繁體（OpenCC 標準）
 
-  * `s2t`: Simplified Chinese to Traditional Chinese
-
-  * `s2tw`: Simplified Chinese to Traditional Chinese (Taiwan standard)
-
-  * `s2twp`: Simplified Chinese to Traditional Chinese (Taiwan standard, with phrases)
-
-  * `t2hk`: Traditional Chinese to Traditional Chinese (Hong Kong standard)
-
-  * `t2s`: Traditional Chinese to Simplified Chinese
-
-  * `t2tw`: Traditional Chinese to Traditional Chinese (Taiwan standard)
-
-  * `tw2s`: Traditional Chinese (Taiwan standard) to Simplified Chinese
-
-  * `tw2sp`: Traditional Chinese (Taiwan standard) to Simplified Chinese (with phrases) 
 
 ## Credits
 [yichen0831](https://github.com/yichen0831/opencc-python) for [opencc-python](https://github.com/yichen0831/opencc-python).
+[BYVoid](https://github.com/BYVoid/OpenCC) for [OpenCC](https://github.com/BYVoid/OpenCC).
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from opencc import OpenCC
+'''pip install opencc-python-reimplemented'''
+# from opencc import OpenCC
+'''now we have official OpenCC binding for Python:
+    pip install opencc'''
+import opencc
 import clipboard
 from wox import Wox, WoxAPI
 
-
 class Main(Wox):
     def copy(self, text):
-
         clipboard.copy(text)
+
+    def convert_opencc_core(text, code):
+        cc = opencc.OpenCC(code)
+        convertedtext = cc.convert(text)
+        # clipboard.copy(convertedtext)
+        return convertedtext
+
+    def selectcode(self, code):
+        WoxAPI.change_query(code)
 
     def query(self, param):
 
@@ -14,43 +25,135 @@ class Main(Wox):
 
         if not param:
             results.append({
-                "Title": '簡繁轉換',
-                "SubTitle": 'Conversions',
+                "Title": '輸入任意文字，即可完成簡繁轉換',
+                "SubTitle": 'opencc',
                 "IcoPath": "Images/icon.png"
             })
+            results.append({
+                "Title": '前綴 "-t2s" 繁體到簡體',
+                "SubTitle": "Traditional Chinese to Simplified Chinese",
+                "IcoPath": "Images/t2s.png",
+                "JsonRPCAction": {
+                        "method": "selectcode",
+                        "parameters": ["cc -t2s "],
+                        "dontHideAfterAction": True
+                        }
+            })
+            results.append({
+                "Title": '前綴 "-s2twp" 簡體到繁體（臺灣正體標準）並轉換為臺灣常用詞彙',
+                "SubTitle": "Simplified Chinese to Traditional Chinese (Taiwan standard, with phrases)",
+                "IcoPath": "Images/s2twp.png",
+                "JsonRPCAction": {
+                        "method": "selectcode",
+                        "parameters": ["cc -s2twp "],
+                        "dontHideAfterAction": True
+                        }
+            })
+            results.append({
+                "Title": '前綴 "-s2tw" 簡體到臺灣正體',
+                "SubTitle": "Simplified Chinese to Traditional Chinese (Taiwan standard)",
+                "IcoPath": "Images/s2tw.png",
+                "JsonRPCAction": {
+                        "method": "selectcode",
+                        "parameters": ["cc -s2tw "],
+                        "dontHideAfterAction": True
+                        }
+            })
+            results.append({
+                "Title": '前綴 "-s2t" 簡體到繁體',
+                "SubTitle": "Simplified Chinese to Traditional Chinese",
+                "IcoPath": "Images/s2t.png",
+                "JsonRPCAction": {
+                        "method": "selectcode",
+                        "parameters": ["cc -s2t "],
+                        "dontHideAfterAction": True
+                        }
+            })
+            results.append({
+                "Title": '前綴 "-tw2sp" 繁體（臺灣正體標準）到簡體並轉換為中國大陸常用詞彙',
+                "SubTitle": "Traditional Chinese (Taiwan standard) to Simplified Chinese (with phrases)",
+                "IcoPath": "Images/tw2sp.png",
+                "JsonRPCAction": {
+                        "method": "selectcode",
+                        "parameters": ["cc -tw2sp "],
+                        "dontHideAfterAction": True
+                        }
+            })
+            results.append({
+                "Title": '前綴 "-t2tw" 繁體（OpenCC 標準）到臺灣正體',
+                "SubTitle": "Traditional Chinese to Traditional Chinese (Taiwan standard)",
+                "IcoPath": "Images/t2tw.png",
+                "JsonRPCAction": {
+                        "method": "selectcode",
+                        "parameters": ["cc -t2tw "],
+                        "dontHideAfterAction": True
+                        }
+            })
+            results.append({
+                "Title": '前綴 "-tw2s" 臺灣正體到簡體',
+                "SubTitle": "Traditional Chinese (Taiwan standard) to Simplified Chinese",
+                "IcoPath": "Images/tw2s.png",
+                "JsonRPCAction": {
+                        "method": "selectcode",
+                        "parameters": ["cc -tw2s "],
+                        "dontHideAfterAction": True
+                        }
+            })           
             return results
-
-        if len(param.split(':')) == 2:
-            code = param.split(':')[1]
-            if code.endswith("s2t"):
-                code = "s2t"
-            elif code.endswith("s2tw"):
-                code = "s2tw"
-            elif code.endswith("s2twp"):
-                code = "s2twp"
-            elif code.endswith("s2twpf"):
-                code = "s2twpf"				
-            elif code.endswith("hk2s"):
-                code = "hk2s"
-            elif code.endswith("s2hk"):
-                code = "s2hk"
-            elif code.endswith("t2hk"):
-                code = "t2hk"
-            elif code.endswith("t2s"):
-                code = "t2s"
-            elif code.endswith("t2tw"):
-                code = "t2tw"
-            elif code.endswith("tw2s"):
-                code = "tw2s"
-            elif code.endswith("tw2sp"):
-                code = "tw2sp"
-            else:
-                code = "s2twp"
-        else:
+        
+        if param.lower().startswith("-s2twp"):
             code = "s2twp"
-
-        cc = OpenCC(code)
-        converted = cc.convert(param.split(':')[0])
+        elif param.lower().startswith("-s2tw"):
+            code = "s2tw"
+        elif param.lower().startswith("-s2t"):
+            code = "s2t"
+        elif param.lower().startswith("-s2hk"):
+            code = "s2hk"
+        elif param.lower().startswith("-hk2s"):
+            code = "hk2s"
+        elif param.lower().startswith("-hk2t"):
+            code = "hk2t"
+        elif param.lower().startswith("-t2s"):
+            code = "t2s"
+        elif param.lower().startswith("-t2hk"):
+            code = "t2hk"
+        elif param.lower().startswith("-t2tw"):
+            code = "t2tw"
+        elif param.lower().startswith("-tw2sp"):
+            code = "tw2sp"
+        elif param.lower().startswith("-tw2s"):
+            code = "tw2s"
+        elif param.lower().startswith("-tw2t"):
+            code = "tw2t"
+        elif param.lower().startswith("-t2jp"):
+            code = "t2jp"
+        elif param.lower().startswith("-jp2t"):
+            code = "jp2t"
+        else:
+            converted1 = Main.convert_opencc_core(param, "s2twp")
+            converted2 = Main.convert_opencc_core(param, "t2s")
+            results.append({
+                    "Title": converted1,
+                    "SubTitle": "s2twp",
+                    "IcoPath": "Images/s2twp.png",
+                    'JsonRPCAction': {
+                            'method': 'copy',
+                            'parameters': [converted1]
+                            }
+                    })
+            results.append({
+                    "Title": converted2,
+                    "SubTitle": "t2s",
+                    "IcoPath": "Images/t2s.png",
+                    'JsonRPCAction': {
+                            'method': 'copy',
+                            'parameters': [converted2]
+                            }
+                    })
+            return results
+        
+        param = param[len(code)+1:len(param)]
+        converted = Main.convert_opencc_core(param, code)
 
         results.append({
             "Title": converted,
